632618be2f4337ac3821de8302943467fd903d20
Nacho Tardajos <itartero@gmail.com>
2026-08-10 20:22:07 +0200

    Add function to generate time travel message

---
+++ custom_module.py
@@ -0,0 +1,3 @@
+# Definir la función para imprimir el mensaje de viaje
+def generate_time_travel_message(year, destination, cost):
+  return f"Pack your bags! You're traveling to {destination} in the year {year}. The cost of this trip will be ${cost}."
