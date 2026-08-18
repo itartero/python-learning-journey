ed0904cab692bdc22f347bda8e8ff8c9ec727853
Nacho Tardajos <itartero@gmail.com>
2026-08-18 17:31:46 +0200

    Add VS Code Python practice

--- vscoode_practice.py
+++ vscoode_practice.py
@@ -1,2 +1,10 @@
 name = "Nacho"
 print(name.capitalize())
+age = 38
+print(age)
+
+def age_calculated(name, age):
+    birth_year = 2026 - age
+    print(f"Hola! Soy {name} y tengo {age} años. Nací en el año {birth_year}")
+
+age_calculated("Nacho", 38)
